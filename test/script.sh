e59747d19a5de81102f6cc2f95ac49a3f7ba5d94
Archit Pandey <architpandeynitk@gmail.com>
2019-04-16 15:39:26 +0530

    Modified test files

--- test/script.sh
+++ test/script.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-echo "" > result.txt
-for i in 1 2 3 4 5 6 7 8 9 10
+dnum=3
+pnum1=1
+pnum2=4
+outfile=result$pnum1-$pnum2.txt
+basepath=../data/dataset$dnum
+
+
+echo "" > $outfile
+for inum1 in 1 2 3
 do
-for j in 1 2 3 4 5 6 7 8 9 10
+for inum2 in 1 2
 do
-	echo "Running script for img$i and img$j"
-	echo "($i,$j)" >> result.txt
-	../build/fingerprint_process ../data/dataset2/img$i.png ../data/dataset2/img$j.png ../build/feature.txt ../build/feature2.txt >> result.txt
+	echo "Running script for $basepath/$pnum1/$inum1.bmp and $basepath/$pnum2/$inum2.bmp"
+	echo "($inum1,$inum2)" >> $outfile
+	../build/fingerprint_process $basepath/$pnum1/$inum1.bmp $basepath/$pnum2/$inum2.bmp ../build/feature.txt ../build/feature2.txt >> $outfile
 done
 done
